6fc7b6eb7ca13fdb240b1ce4060cb340b69e939a
Nikolay Nuzhnyh <niknuzhnyh@gmail.com>
2023-07-08 08:00:18 +0300

    Update test.1.js

--- test.1.js
+++ test.1.js
@@ -3,8 +3,8 @@ function gAH(params) {
     console.log(params);
     console.log(parseJwt(params.credential));
     let parsedToken = parseJwt(params.credential)
-    document.getElementById('outPutName').innerText(`name is ${parsedToken.name}`)
-    document.getElementById('outPutEmail').innerText(`email is ${parsedToken.email}`)
+    document.getElementById('outPutName').innerText = `name is ${parsedToken.name}`
+    document.getElementById('outPutEmail').innerText = `email is ${parsedToken.email}`
 }
 
 function parseJwt(token) {
