59a51e4e62e678da54931747d0724664c9fe9da6
Nikolay Nuzhnyh <niknuzhnyh@gmail.com>
2023-07-21 09:33:14 +0300

    Update test.1.js

--- test.1.js
+++ test.1.js
@@ -37,4 +37,63 @@ for (const i of mainScreenBtns) {
   });
 }
 
+let obj =   {
+  "pacientName": "Закутинський Віктор Федорович",
+  "place": "КНП ОБЛАСНА КЛІНІЧНА ЛІКАРНЯ ІМ.О.Ф.ГЕРБАЧЕВСЬКОГО ЖИТОМИРСЬКОЇ ОБЛАСНОЇ РАДИ",
+  "citi": "Житомир",
+  "diagnosis": "ВОСП правого колінного суглобу з переломом надколінника з внутрішньосуглобовим переломом латерального мищелка стегнової кістки, гемартроз обширний забій м'яких тканин. ВОСП правої кісті з неповною травматичною ампутацією 3-4 пальців дистальних фаланг. ВОСП шиї з наявністю стороннього металевого тіла на рівні С4. Множинні ВОС обличча.",
+  "distCode": "3.3.3.1",
+  "icdCode": "Y56.0",
+  "lossCode": "1.2.1",
+  "startDate": " 5/6/2005 09:34:42 PM"
+}
+
+function dateTimeFormta(date) {
+  let d = new Intl.DateTimeFormat("uk-UA", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+  date = Date.parse(date);
+  date = d.format(date);
+  return date;
+}
+
+
+function pacientListItemRender(obj) {
+  let date = new Date(obj.startDate)
+  date = dateTimeFormta(date)
+  let str = `                    
+  <div class="pacientList_item">
+    <div class="pacientList_item_name">
+        ${obj.pacientName}
+    </div>
+    <div class="pacientList_item_plase">
+        ${obj.place}
+    </div>
+    <div class="pacientList_item_diagnosis">
+        ${obj.diagnosis}
+    </div>
+    <div class="pacientList_item_footer df jcsb">
+        <div class="pacientList_item_date">
+          ${date}
+        </div>
+        <div class="pacientList_item_lossCode">
+            ${obj.lossCode} ( <span class="pacientList_item_icdCode">${obj.icdCode}</span> )
+        </div>
+    </div>
+  </div>`
+
+  return str
+
+}
+
+let div = document.createElement('div');
+div.innerHTML = pacientListItemRender(obj)
+
+document.getElementById('pacientList').appendChild(div)
+
 console.log("ok");
